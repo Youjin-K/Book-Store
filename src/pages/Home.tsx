5e9c0e080cbff3ac80e2523c231003856c208465
Youjin Kim <gopar0101@gmail.com>
2024-11-11 15:54:41 +0900

    first

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <div>Home body</div>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
